0da55176629e47e813a3f89b3b05ceaf75b5187e
Charmed Goose <Charmedgoose1@gmail.com>
2022-06-29 09:18:18 -0400

    Docker

--- cogs/General.py
+++ cogs/General.py
@@ -22,9 +22,7 @@ class General(commands.Cog):
     @commands.Cog.listener()
     async def on_member_join(self, member):
         channel = await self.client.fetch_channel("973909111471235082")
-        await channel.send("Since <@465097356874874881> Won't Do It I Will")
-        await asyncio.sleep(10)
-        await channel.send(f'Welcome <@{member.id}>\n Take <#973911546226962463>')
+        await channel.send(f'Welcome <@{member.id}>\nTake <#973911546226962463>')
 
     @commands.Cog.listener()
     async def on_message_delete(self, message):
